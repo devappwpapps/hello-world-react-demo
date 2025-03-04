563365319bda28b7fb2ec35313ac333a83c5f1fa
Shyam <shyam@wpapps.press>
2025-03-04 18:01:49 +0530

    worked on creating Hello World React app

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="app-container">
+      <div className="content-box">
+        <h1 className="main-heading">Hello World</h1>
+        <p className="sub-text">Welcome to our modern React application</p>
+        <div className="decorative-line"></div>
+        <p className="description">
+          This is a professional-looking React app built with TypeScript.
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
